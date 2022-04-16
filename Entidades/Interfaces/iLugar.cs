5494b67c55db6a04aa8485cba99465729d548cd5
jimmy-20 <jasocanal@gmail.com>
2022-04-16 00:25:03 -0600

    Se agrego una nueva interfaz

---
+++ Entidades/Interfaces/iLugar.cs
@@ -0,0 +1,11 @@
+namespace CoreEscuela.Entidades.Interfaces
+{
+    public interface iLugar
+    {
+        string Direccion { get; set; }
+        void LimpiarLugar();
+    }
+
+    //Una interfaz es una definicion de la estructura
+    //que debe contener un objeto
+}
